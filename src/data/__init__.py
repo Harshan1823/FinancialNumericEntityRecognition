f0c0c23e8477dfa1a01f16e0f25ef35a380d04af
Harshan Ganugula <harshan1823@gmail.com>
2023-10-01 04:10:03 +0530

    Updated .gitignore and added functions to download data from HuggingFace

---
+++ src/data/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+# Define the relative path to the destination folder
+DESTINATION_FOLDER = Path(__file__).parent.parent.parent / 'data' / 'raw'
+DATASET_NAME = "nlpaueb/finer-139"
