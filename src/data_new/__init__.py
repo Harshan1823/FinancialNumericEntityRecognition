d5f820a46103ed5b4160d5704d05c1ab396427b3
Harshan Ganugula <harshan@Harshans-MacBook-Pro.local>
2023-11-02 21:03:17 -0400

    Added new data pre-processing functions

---
+++ src/data_new/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+# Define the relative path to the destination folder
+DATA_FOLDER = Path(__file__).parent.parent.parent / 'data' 
+DESTINATION_FOLDER = Path(__file__).parent.parent.parent / 'data' / 'raw'
+DATASET_NAME = "nlpaueb/finer-139"
